87a7541fe7ea388959e6f95fad1eef81cafd17e8
mariia barchenkova <mary.iawq@gmail.com>
2025-08-14 16:48:11 +0700

    feat: add PT Root UI fonts woff for legacy browser support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt']
+  modules: ['@pinia/nuxt'],
+  css: [
+    '~/assets/styles/fonts.sass',
+    '~/assets/styles/main.sass',
+  ],
 })
